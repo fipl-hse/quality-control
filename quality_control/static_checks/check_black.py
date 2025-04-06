4ff6b81469a71a0a1982671a1115a0d95ad6707b
EIzharikov <egor.zharikov@inbox.ru>
2025-04-06 14:55:52 +0300

    Config files for black

--- quality_control/static_checks/check_black.py
+++ quality_control/static_checks/check_black.py
@@ -5,10 +5,10 @@ Check black to check the style and quality of Python code.
 # pylint: disable=duplicate-code
 from pathlib import Path
 
-from config.cli_unifier import _run_console_tool, choose_python_exe, handles_console_error
-from config.console_logging import get_child_logger
-from config.constants import PROJECT_CONFIG_PATH, PROJECT_ROOT
-from config.project_config import ProjectConfig
+from quality_control.cli_unifier import _run_console_tool, choose_python_exe, handles_console_error
+from quality_control.console_logging import get_child_logger
+from quality_control.constants import PROJECT_CONFIG_PATH, PROJECT_ROOT
+from quality_control.project_config import ProjectConfig
 
 logger = get_child_logger(__file__)
 
