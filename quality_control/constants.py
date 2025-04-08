70a246b9b580dec830282affb8b9c1fd53a414fc
EIzharikov <egor.zharikov@inbox.ru>
2025-04-08 21:38:51 +0300

    path?

--- quality_control/constants.py
+++ quality_control/constants.py
@@ -8,4 +8,4 @@ PROJECT_ROOT = Path(__file__).parent.parent.parent.parent.parent
 PROJECT_CONFIG_PATH = PROJECT_ROOT / "project_config.json"
 CONFIG_PACKAGE_PATH = PROJECT_ROOT / "config"
 CORE_UTILS_PACKAGE_PATH = PROJECT_ROOT / "core_utils"
-ABSOLUTE_CWD_PATH = Path.cwd().resolve()
+ABSOLUTE_CWD_PATH = Path.cwd()
