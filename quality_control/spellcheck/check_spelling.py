b1a98c97a8e13e8192889d2352240d7282d9f4e1
EIzharikov <egor.zharikov@inbox.ru>
2025-09-21 00:38:56 +0300

    new spellcheck

--- quality_control/spellcheck/check_spelling.py
+++ quality_control/spellcheck/check_spelling.py
@@ -7,6 +7,7 @@ import os
 from pathlib import Path
 import re
 import sys
+from typing import Pattern
 
 from quality_control.cli_unifier import _run_console_tool, choose_python_exe, handles_console_error
 from quality_control.console_logging import get_child_logger
@@ -31,14 +32,43 @@ def check_spelling_on_paths(root_dir: Path) -> tuple[str, str, int]:
 
     return _run_console_tool(str(choose_python_exe(lab_path=root_dir)), spelling_args, debug=True, cwd=root_dir)
 
+def get_misspelled_from_stdout(stdout: str, additional_re_check: Pattern | None = None) -> set[str]:
+    """
+    Get words from the blocks of pyspelling output.
+
+    Args:
+        stdout (str): stdout log
+
+    Returns:
+        set[str]: set of misspelled words
+    """
+    pattern = re.compile(
+        r"Misspelled words:\n<[a-zA-Z_-]+> .*: .*\n-+(?P<wrong>(([а-яА-ЯёЁa-zA-Z\-]{1,})\n?)+)"
+    )
+
+    all_misses = set()
+    logger.info("Parsing words from the log.")
+    for found in pattern.finditer(stdout):
+        all_misses.update(
+            [
+                word.lower()
+                for word in found.group("wrong").strip().split("\n")
+                if word and len(word) != 80
+            ]
+        )
+    if additional_re_check is None:
+        return all_misses
+
+    return {word for word in all_misses if additional_re_check.search(word)}
 
 def main() -> None:
     """
     Run spellchecking for the project.
     """
 
-    SPELLCHECK_DIR = files(spellcheck)
-    os.environ["SPELLCHECK_DIR"] = str(SPELLCHECK_DIR)
+    russian_word_p = re.compile(r"[а-яА-ЯёЁ]+")
+    english_word_p = re.compile(r"[a-zA-Z]+")
+
     args = BlackArgumentsParser().parse_args()
 
     root_dir = args.root_dir.resolve()
@@ -46,32 +76,32 @@ def main() -> None:
 
     fileConfig(toml_config)
 
-    stdout, _, return_code = check_spelling_on_paths(root_dir=root_dir)
-    if return_code == 0:
-        logger.info("Spelling check is passed.")
-        sys.exit(0)
-
-    pattern = re.compile(
-        r"Misspelled words:\n<htmlcontent>[ a-zA-Z_\/\.0-9]+:\s([a-zA-Z\.0-9]+>?)+\n-+\n"
-        r"(?P<wrong>(([а-яА-ЯёЁa-zA-Z\-]{1,})\n?)+)"
+    stdout, _, return_code = check_spelling_on_paths("ru")
+    missed_russian = (
+        set(get_misspelled_from_stdout(stdout, russian_word_p)) if return_code else set()
     )
 
-    final = []
-    for found in pattern.finditer(stdout):
-        final.extend(
-            [
-                word.lower()
-                for word in found.group("wrong").strip().split("\n")
-                if word and len(word) != 80
-            ]
-        )
-    missed = sorted(set(final))
+    stdout, _, return_code = check_spelling_on_paths("en")
+    missed_english = (
+        set(get_misspelled_from_stdout(stdout, english_word_p)) if return_code else set()
+    )
 
-    if missed:
-        logger.info("List of potentially wrong words:")
-        logger.info("\n\n" + "\n".join(missed))
+    stdout, _, return_code = check_spelling_on_paths("docstrings")
+    missed_docstrings = set(get_misspelled_from_stdout(stdout)) if return_code else set()
 
-    logger.error("Spellcheck is not passed.")
+    if not missed_docstrings and not missed_russian and not missed_english:
+        logger.info("Spelling: OK")
+        sys.exit(0)
+
+    if missed_english or missed_russian:
+        logger.info("List of potentially wrong words in docs:")
+        logger.info("\n\n" + "\n".join(sorted(missed_english | missed_russian)) + "\n")
+
+    if missed_docstrings:
+        logger.info("List of potentially wrong words in docstrings:")
+        logger.info("\n\n" + "\n".join(sorted(missed_docstrings)) + "\n")
+
+    logger.error("Spelling: FAIL")
     sys.exit(1)
 
 
