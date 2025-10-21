9eefd880a7858574ee1a57b6d589d60e3cc1bd5f
QuietHellsPage <baazhinga@yandex.ru>
2025-10-21 20:39:18 +0300

    Add flag

--- quality_control/quality_control_parser.py
+++ quality_control/quality_control_parser.py
@@ -9,3 +9,4 @@ class QualityControlArgumentsParser(Tap):
     toml_config_path: Optional[Path] = None
     root_dir: Optional[Path] = Path(os.getcwd())
     project_config_path: Optional[Path] = None
+    ignore_tests: Optional[bool] = False
