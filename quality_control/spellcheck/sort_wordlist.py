bc238d553ce398f0bf7573463bbf4a7885234d76
Zharikov Egor <90442695+EIzharikov@users.noreply.github.com>
2025-12-02 15:22:33 +0300

    [TOOLS] Sorting wordlist is enabled (#18)

--- quality_control/spellcheck/sort_wordlist.py
+++ quality_control/spellcheck/sort_wordlist.py
@@ -5,7 +5,10 @@ Module to check if wordlist is properly sorted.
 import re
 from pathlib import Path
 
+from logging518.config import fileConfig
+
 from quality_control.console_logging import get_child_logger
+from quality_control.quality_control_parser import QualityControlArgumentsParser
 
 logger = get_child_logger(__file__)
 
@@ -47,8 +50,15 @@ def main() -> None:
     """
     Call functions.
     """
-    main_wordlist_path = Path(__file__).parent / ".wordlist.txt"
-    secondary_wordlist_path = Path(__file__).parent / ".wordlist_en.txt"
+    args = QualityControlArgumentsParser(underscores_to_dashes=True).parse_args()
+    
+    root_dir = args.root_dir.resolve()
+    toml_config = (args.toml_config_path or (root_dir / "pyproject.toml")).resolve()
+    fileConfig(toml_config)
+    
+    main_wordlist_path = root_dir / "admin_utils" / "spellcheck"  / ".wordlist.txt"
+    secondary_wordlist_path = root_dir / "admin_utils" / "spellcheck"  / ".wordlist_en.txt"
+
 
     for current_path in (main_wordlist_path, secondary_wordlist_path):
         if current_path.exists():
