d45d7080c4043e7d911d0423660297456553b274
AnPruch <134655389+AnPruch@users.noreply.github.com>
2025-12-09 15:26:25 +0300

    [TOOLS] Docstrings check fix (#20)

--- quality_control/static_checks/check_docstrings.py
+++ quality_control/static_checks/check_docstrings.py
@@ -6,10 +6,7 @@ from pathlib import Path
 
 from logging518.config import fileConfig
 
-from quality_control.cli_unifier import (
-    _run_console_tool,
-    handles_console_error,
-)
+from quality_control.cli_unifier import _run_console_tool, handles_console_error
 from quality_control.console_logging import get_child_logger
 from quality_control.constants import PROJECT_ROOT
 from quality_control.static_checks.check_black import QualityControlArgumentsParser
@@ -28,7 +25,7 @@ def check_with_pydoctest(path_to_config: Path, root_dir: Path) -> tuple[str, str
     Returns:
         tuple[str, str, int]: stdout, stderr, exit code
     """
-    pydoctest_args = ["--config", str(path_to_config), "--verbosity", "2"]
+    pydoctest_args = ["--config", str(path_to_config)]
     return _run_console_tool("pydoctest", pydoctest_args, debug=True, cwd=root_dir)
 
 
@@ -37,8 +34,15 @@ def main() -> None:
     Check docstrings for labs, config and core_utils packages.
     """
     args = QualityControlArgumentsParser(underscores_to_dashes=True).parse_args()
+
     root_dir = args.root_dir.resolve()
-    pydoctest_path = (PROJECT_ROOT / "static_checks" / "pydoctest.json").resolve()
+
+    pydoctest_path = (
+        (root_dir / "pydoctest.json")
+        or (PROJECT_ROOT / "static_checks" / "pydoctest.json")
+    ).resolve()
+    if args.project_config_path:
+        pydoctest_path = args.project_config_path.resolve()
 
     toml_config = (args.toml_config_path or (root_dir / "pyproject.toml")).resolve()
     fileConfig(toml_config)
