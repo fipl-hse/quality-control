092abc5345f0967e6e84c97d6b656cd35b908285
EIzharikov <egor.zharikov@inbox.ru>
2026-01-04 22:52:23 +0300

    [CI] Generate stubs

--- quality_control/generate_stubs/generate_labs_stubs.py
+++ quality_control/generate_stubs/generate_labs_stubs.py
@@ -12,6 +12,13 @@ from quality_control.generate_stubs.run_generator import (
     sort_stub_imports,
 )
 from quality_control.project_config import ProjectConfig
+import re
+from pathlib import Path
+
+from logging518.config import fileConfig
+
+from quality_control.console_logging import get_child_logger
+from quality_control.quality_control_parser import QualityControlArgumentsParser
 
 logger = get_child_logger(__file__)
 
@@ -36,7 +43,7 @@ def _generate_stubs_single_module(
     sort_stub_imports(stub_path)
 
 
-def generate_all_stubs(project_config: ProjectConfig) -> None:
+def generate_all_stubs(project_config: ProjectConfig, root_dir: Path) -> None:
     """
     Generate stubs for all labs.
 
@@ -58,17 +65,28 @@ def generate_all_stubs(project_config: ProjectConfig) -> None:
         logger.info(f"Generating stubs for {lab_name}.")
 
         for filename in stubs_list:
-            module_path = PROJECT_ROOT / lab_name / filename
+            module_path = root_dir / lab_name / filename
             logger.info(f"Processing file {filename} -> {module_path}")
-            _generate_stubs_single_module(module_path, PROJECT_ROOT, project_config)
+            _generate_stubs_single_module(module_path, root_dir, project_config)
 
 
 def main() -> None:
     """
     Entrypoint for stub generation.
     """
-    proj_conf = ProjectConfig(PROJECT_CONFIG_PATH)
-    generate_all_stubs(proj_conf)
+    args = QualityControlArgumentsParser(underscores_to_dashes=True).parse_args()
+
+    root_dir = args.root_dir.resolve()
+    toml_config = (args.toml_config_path or (root_dir / "pyproject.toml")).resolve()
+
+    project_config_path = (
+        args.project_config_path or (root_dir / "project_config.json")
+    ).resolve()
+
+    project_config = ProjectConfig(project_config_path)
+
+    fileConfig(toml_config)
+    generate_all_stubs(project_config, root_dir)
 
 
 if __name__ == "__main__":
