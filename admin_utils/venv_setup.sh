b86bb38430bcccdaf611b514736f28ca960b51f5
Yuztrazure <artemijsidnev04923@gmail.com>
2026-08-31 11:54:49 +0300

    fix: ci

---
+++ admin_utils/venv_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ex
+
+python -m venv venv
+source venv/bin/activate
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python -m pip install -r requirements_qa.txt
